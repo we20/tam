c5d8073c0e14f4ed4ea8f03461d2e319711aeefa
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-26 11:13:08 -0800

    Create fluxterspm.lua

---
+++ plugins/fluxterspm.lua
@@ -0,0 +1,21 @@
+do 
+
+function run(msg, matches)
+  return "
+  end
+  
+  return {
+  description = "a plugin for FluxterTeam", 
+  usage = {
+  "!fluxterteam : fluxterspam",
+  "fluxterspm : fluxterteam spam",
+  patterns = {
+    "^[/](",
+    "^fluxterspm$",
+
+  },
+  run = run
+}
+
+end
+  
