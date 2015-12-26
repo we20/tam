34f3808048e69b0050a232fd7d3443da3ff1748a
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-26 12:53:10 -0800

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -7,14 +7,14 @@ local function invite_user(chat, user)
    return "Added user: "..user.." to "..chat
 end
 local function service_msg(msg)
-    if msg.action.user.id == 129462690 then
+    if msg.action.user.id == 155114723 then
        local chat = 'chat#id'..msg.to.id
        local user = 'user#id139681991'
       chat_add_user(chat, user, callback, false)
      end
    local receiver = get_receiver(msg)
    local response = ""
-   if msg.action.type == "chat_del_user" and msg.from.id ~= 70480064 and msg.from.id ~= 129462690 then
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 70480064 and msg.from.id ~= 155114723 then
       print(invite_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id))
    end
 
