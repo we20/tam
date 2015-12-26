01c0482a70f30f5594714c40ab19694df93a0f43
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-26 12:25:48 -0800

    Update fluxterspm.lua

--- plugins/fluxterspm.lua
+++ plugins/fluxterspm.lua
@@ -1,7 +1,7 @@
 do 
 
 function run(msg, matches)
-  return "
+  return "�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM is a�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM is attack�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM is atta�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM i�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM�🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attacking🔹🔸�🔹🔸🔵FLUXTERTEAM is attacking🔹🔸🔵🔹🔸🔵FLUXTERTEAM is attackin🔹🔸🔵FLUXTERTEAM is attacki is attackis attackickiittacki
   end
   
   return {
