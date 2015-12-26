319ba589a58a6284b29fbb38d019f0c2b552e69a
FLUXTERTEAM <arashmardan.am@gmail.com>
2015-12-26 05:14:47 -0800

    Create getlink.lua

---
+++ plugins/getlink.lua
@@ -0,0 +1,32 @@
+ if matches[1] == 'clink' then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local function callback (extra , success, result)
+        local receiver = 'chat#'..msg.to.id
+        if success == 0 then
+           return send_large_msg(receiver, '*Error: Invite link failed* \nReason: Not creator.')
+        end
+        send_large_msg(receiver, "Created a new link!")
+        data[tostring(msg.to.id)]['settings']['set_link'] = result
+        save_data(_config.moderation.data, data)
+      end
+      local receiver = 'chat#'..msg.to.id
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] revoked group link ")
+      return export_chat_link(receiver, callback, true)
+    end
+    if matches[1] == 'glink' then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local group_link = data[tostring(msg.to.id)]['settings']['set_link']
+      if not group_link then 
+        return "Create a link using /newlink first !"
+      end
+       savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
+      return "The invite link for this groups is :\n"..group_link
+    end
+    
+     patterns = {
+       "^[!/](clink)$",
+  "^[!/](glink)$",
