9509f3468a3d965542129972f56d9ee3bcb09404
hacker4moon <0.creed.is.dead.0@gmail.com>
2015-12-29 12:37:17 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -147,7 +147,7 @@ local function run(msg, matches)
       end
     end
 
-    if matches[2] == "teledark" then
+    if matches[2] == "creedbot" then
       if not is_sudo(msg) then
         return "Bot stats requires privileged user"
       else
@@ -175,7 +175,7 @@ return {
   patterns = {
     "^!([Ss]tats)$",
     "^!([Ss]tats) (chat) (%d+)",
-    "^!([Ss]tats) (teledark)"
+    "^!([Ss]tats) (creedbot)"
     }, 
   run = run,
   pre_process = pre_process
