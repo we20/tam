5fab5d7e7705c619b3787ef9538b96b1111d9773
hacker4moon <0.creed.is.dead.0@gmail.com>
2015-12-29 12:54:06 +0330

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -53,7 +53,7 @@ do
       local data = load_data(_config.moderation.data)
 
       -- create a group
-      if matches[1] == 'mkgroup' and matches[2] and is_mod(msg) then
+      if matches[1] == 'cgroup' and matches[2] and is_mod(msg) then
         create_group_chat (msg.from.print_name, matches[2], ok_cb, false)
 	      return 'Group '..string.gsub(matches[2], '_', ' ')..' has been created.'
       -- add a group to be moderated
@@ -360,7 +360,7 @@ do
     description = 'Plugin to manage group chat.',
     usage = {
       admin = {
-        '!mkgroup <group_name> : Make/create a new group.',
+        '!cgroup <group_name> : Make/create a new group.',
         '!addgp : Add group to moderation list.',
         '!remgp : Remove group from moderation list.'
       },
